3509690890662d80550cc3a79a7c108c065ee698
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-05-24 21:01:34 +0100

    Add The Source File & The Test File & The Python Script File For The add_integer function

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,39 @@
+# 0-add_integer.py
+
+def add_integer(a, b=98):
+    """
+    Add two integers.
+
+    Args:
+        a: The first integer.
+        b: The second integer, default is 98.
+
+    Returns:
+        The sum of a and b.
+
+    Raises:
+        TypeError: If a or b are not integers or floats.
+
+    Examples:
+        >>> add_integer(1, 2)
+        3
+        >>> add_integer(100, -2)
+        98
+        >>> add_integer(2)
+        100
+        >>> add_integer(100.3, -2)
+        98
+        >>> add_integer(4, "School")
+        Traceback (most recent call last):
+            ...
+        TypeError: b must be an integer
+        >>> add_integer(None)
+        Traceback (most recent call last):
+            ...
+        TypeError: a must be an integer
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
