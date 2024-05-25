3db6bfba5473fe9554d229639ee38802c2870e65
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-05-25 21:24:57 +0100

    Animal Class

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,43 +1,44 @@
 from abc import ABC, abstractmethod
 from math import pi
 
+
 class Shape(ABC):
     @abstractmethod
     def area(self):
         pass
-    
+
     @abstractmethod
     def perimeter(self):
         pass
 
+
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
-    
+
     def area(self):
-        if self.radius < 0:
-            raise ValueError("Radius must be a non-negative number")
         return pi * self.radius ** 2
-    
+
     def perimeter(self):
-        if self.radius < 0:
-            raise ValueError("Radius must be a non-negative number")
         return 2 * pi * self.radius
+
 
 class Rectangle(Shape):
     def __init__(self, width, height):
         self.width = width
         self.height = height
-    
+
     def area(self):
         return self.width * self.height
-    
+
     def perimeter(self):
         return 2 * (self.width + self.height)
+
 
 def shape_info(shape):
     print("Area:", shape.area())
     print("Perimeter:", shape.perimeter())
+
 
 if __name__ == "__main__":
     circle = Circle(radius=5)
