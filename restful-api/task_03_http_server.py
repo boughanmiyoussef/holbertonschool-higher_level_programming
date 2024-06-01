73eb46905701e5538deea0683dabde0b3c8e89db
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-06-01 20:13:05 +0100

    c

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,7 +7,7 @@ import http.server
 import json
 
 
-class SimpleServer(http.server.BaseHTTPRequestHandler):
+class ServerRunner(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
@@ -29,15 +29,15 @@ class SimpleServer(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            self.wfile.write(b"404 Not Found")
 
 
-def run_server():
+def execute():
     server_address = ('', 8000)
-    httpd = http.server.HTTPServer(server_address, SimpleServer)
+    httpd = http.server.HTTPServer(server_address, ServerRunner)
     print("Server running on port 8000...")
     httpd.serve_forever()
 
 
 if __name__ == "__main__":
-    run_server()
+    execute()
