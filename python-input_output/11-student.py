9f43e89679d0eca3856e8457832c7d156dc2cf7e
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-06-01 14:02:15 +0100

    class

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -3,11 +3,10 @@
 
 
 class Student:
-    """Represent a student."""
+    """Represents a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
-        """
+        """Initializes a new Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -15,16 +14,14 @@ class Student:
     def to_json(self, attrs=None):
         """Gets a dictionary representation of the Student.
         If attrs is a list of strings, represents only those attributes
-        included in the list
+        included in the list.
         """
-
-
-if (isinstance(attrs, if ()                all(type(ele) == str for ele in attrs)):
+        if isinstance(attrs, list) and all(isinstance(ele, str)
+                                           for ele in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student
-        """
+        """Replaces all attributes of the Student with those in the json dictionary"""
         for k, v in json.items():
             setattr(self, k, v)
