4303ea093f828309732b6f89c74e29d2e620ba95
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-06-01 14:44:48 +0100

    HTTP Server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -39,7 +39,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'Endpoint not found')
+            self.wfile.write(b'Not Found')
 
 
 def run(server_class=http.server.HTTPServer,
