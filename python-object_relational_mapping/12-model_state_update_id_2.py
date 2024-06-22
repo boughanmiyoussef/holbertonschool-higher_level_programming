4bea51e8d4791c3ba362f6e3fcdffc9201b7eb09
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-06-22 21:15:18 +0100

    update

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,27 +1,22 @@
-
 #!/usr/bin/python3
-"""Updates into State obj from db"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+"""changes the name of a State object from
+the database hbtn_0e_6_usa"""
 
+if __name__ == "__main__":
 
-def update_to_state_obj():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-
-    to_change = session.query(State).filter(State.id == 2).first()
-
-    to_change.name = 'New Mexico'
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
     session.commit()
-
     session.close()
-
-
-if __name__ == "__main__":
-    update_to_state_obj()
