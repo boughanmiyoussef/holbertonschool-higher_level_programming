9b3c519e648afe4cbf2072678aabe22f7c4607e5
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-06-22 21:13:02 +0100

    insert

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,25 +1,26 @@
-
 #!/usr/bin/python3
-"""adds the State object “Louisiana”
-to the database hbtn_0e_6_usa"""
+"""Inserts into State obj from db"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
 
-if __name__ == "__main__":
 
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+def insert_to_state_obj():
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    new = State(name='Louisiana')
-    session.add(new)
-    new_state = session.query(State).filter(State.name == 'Louisiana').first()
+    state = State(name='Louisiana')
+
+    session.add(state)
     session.commit()
-    print("{}".format(new_state.id))
+    print(state.id)
+
     session.close()
+
+
+if __name__ == "__main__":
+    insert_to_state_obj()
