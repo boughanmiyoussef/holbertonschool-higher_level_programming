3b812031b654c75f93e3393d62e3590f31cb6925
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-06-22 21:04:19 +0100

    Fetch All

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,36 +1,26 @@
-
 #!/usr/bin/python3
-"""
-Script that lists all lists all `State`
-objects from the database `hbtn_0e_6_usa`.
-
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
-"""
-
+"""List all State objects from db"""
 import sys
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy.engine.url import URL
 from model_state import Base, State
 
 
-if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
-
-    engine = create_engine(URL(**url), pool_pre_ping=True)
+def list_state_obj():
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(bind=engine)
+    session = Session(engine)
 
-    q = session.query(State).order_by(State.id)
+    rows = session.query(State).all()
 
-    for instance in q:
-        print("{}: {}".format(instance.id, instance.name))
+    for i in rows:
+        print("{}: {}".format(i.__dict__['id'], i.__dict__['name']))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    list_state_obj()
